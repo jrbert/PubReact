e26d67f56da064beb135da7ab20742d817abf86d
Allan Lima <allan.r.lima2017@gmail.com>
2021-08-27 17:00:26 -0300

    deletando Menu

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,33 +1,21 @@
-import React, {Component} from 'react'
+import React from 'react'
 import './App.css'
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Header from './Header/Header'
-import Menu from '/sty'
 
- const App = () => {
+
+const App = () => {
  
-  
     return (
       <div className="App">
         <Router>
-       
-          <Header />
-<<<<<<< HEAD
-        </Route>
-        <Switch>
-        <Button />
-        </Switch>
-=======
-      
->>>>>>> e8322caceb7254d72c49cf79d47ecca417eefacd
+          
+            <Header />
+          
         </Router>
       </div>
-  
-      
-      
-  
     
     );
   
 }
-export default App
+export default App;
--- my-app/src/Header/Header.jsx
+++ my-app/src/Header/Header.jsx
@@ -1,15 +1,15 @@
 //packages
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 //styles 
-import {Menu} from './Header.styles.js'
+import { Menu } from './Header.styles.js'
 
 const Header = () => {
     return (
-        <Menu class="">
+        <Menu>
             <nav>
                 <ul>
-                    <li id = 'resiliapub'><Link to="/resilia-pub">Resilia Pub</Link></li>
+                    <li id="resiliapub"><Link to="/resilia-pub">Resilia Pub</Link></li>
                     <li><Link to="/cardapio">Cardápio</Link></li>
                     <li><Link to="/musicas">Músicas</Link></li>
                     <li><Link to="/equipe">Nossa Equipe</Link></li>
