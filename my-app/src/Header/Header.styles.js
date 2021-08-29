ace517ffa51ea0d1eff11bf26345432789781306
jrbert <cabfjunior@gmail.com>
2021-08-28 22:06:50 -0300

    mudanças na pagina home

--- my-app/src/Header/Header.styles.js
+++ my-app/src/Header/Header.styles.js
@@ -3,6 +3,8 @@ import styled from 'styled-components'
 export const Menu = styled.header`
 
 @import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;1,100;1,400&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;0,600;1,400;1,600&display=swap%27');
+
 display: flex;
 background: #0F1E1D;
 
@@ -18,15 +20,19 @@ ul {
     justify-content: space-around;
     
 }
-li a {
-    font-size: 18px;
-    font-family: 'Lato', sans-serif;
-    text-decoration: none;
-    color: #5AEBD1;
-}
+
 #resiliapub {
+    font-family: 'Playfair Display', serif;
     font-weight: bold;
    
 }
+
+li a {
+    font-size: 18px;
+    font-family: 'Playfair Display', serif;
+    text-decoration: none;
+    color: #5AEBD1;
+}
+
 `
 
