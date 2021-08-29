44f99fbc8120274e66f89fe17a2ea837d81f71b0
Allan Lima <allan.r.lima2017@gmail.com>
2021-08-28 21:34:48 -0300

    Component Main, Grid e Route home

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,47 +7,22 @@ import Footer from './Footer/Footer.jsx'
 import Main from './Main/Main.jsx'
 
 const App = () => {
-<<<<<<< HEAD
-
-  
-    return (
-      <div className="App">
-        <Router>
-          <Switch>
-            <Header />
-              <Route path="/">
-                <Main />
-              </Route>  
-              <Route path="/musicas">
-                <RenderMusic />
-              </Route>
-            </Switch>
-          </Router>
-        <div>
-          <Footer />
-        </div>    
-      </div>
-  )
-=======
   return (
     <div className="App">
       <Router>
         <Header />
-        <Switch>
-          <Route path="/" exact>
+          <Switch>
+            <Route path="/">
               <Main />
-          </Route>  
-        </Switch>
-        <Switch>
-          <Route path="/musicas">
-            <RenderMusic/>
-          </Route>
-        </Switch>
+            </Route>  
+            <Route path="/musicas">
+              <RenderMusic />
+            </Route>
+          </Switch>
+        <Footer />
       </Router>
-      <Footer />
     </div>
   );
->>>>>>> a4ca95dcbc6c786c807a0a7db702c8ced25ccb76
 }
 
 
--- my-app/src/Main/Main.styles.js
+++ my-app/src/Main/Main.styles.js
@@ -1,11 +1,69 @@
 import styled from 'styled-components'
 
-export const ImagemPub = styled.main`
+export const Conteudo = styled.main`
+@import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;1,100;1,400&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;0,600;1,400;1,600&display=swap%27');
 
-    display: grid;
-    grid: 150px / auto auto auto;
-    background-color: red;
 
+        margin: 0; 
+        padding: 0;
+        box-sizing: border-box;
+        background: #7159c1;
+        display: grid;
+        grid-template-columns: 3fr 1fr;
+        grid-template-rows: auto;
+    
+    
+    #imagemprincipal {
+        display: inline-block;
+        grid-column-start: 1;
+        grid-column-end: 4;
+        height: 500px;
+    
+    }
+    #foto {
+        width: 100%;
+    }
+
+    #figcap {
+        position: absolute;   
+        top: 145px;   
+        right: 20px;   
+        font-family: 'Playfair Display', serif;
+        font-size: 85px;   
+        color: #5AEBD1;;   
+        text-shadow: 0px 0px 5px black;
+
+
+        //position: relative;
+        //font-size: 20px;
+        //color: #fff;
+        //text-align: center;
+        //vertical-align: middle;
+    }
+
+    #pratos {
+        grid-column-start: 1;
+        grid-column-end: 2;
+        background: green;
+        height: 60vh;
+    }
+
+    #aside {
+        grid-column-start: 2;
+        grid-column-end: 3;
+        grid-row-start: 2;
+        grid-row-end: 4;
+        background: blue;
+        
+    }
+
+    #info {
+        grid-column-start: 1;
+        grid-column-end: 2;
+        background: pink;
+        height: 100px;
+}
 `
 
 
