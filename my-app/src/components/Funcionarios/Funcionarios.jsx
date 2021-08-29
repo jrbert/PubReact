675eb95fe63dd9af790477bf4fe7a0fe510cdea8
GlaucoTolotto <glaucotolotto@gmail.com>
2021-08-29 03:20:41 -0300

    Estilo da pagina de funcionarios iniciado

--- my-app/src/components/Funcionarios/Funcionarios.jsx
+++ my-app/src/components/Funcionarios/Funcionarios.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import {Main} from './Funcionarios.styles.js'
 function Funcionarios ({nome, github, linkedin}){
     return(
-        <div>
+        <Main>
             <h2>{nome}</h2>
-            <p>Github:<a>{github}</a></p>
-            <p>Linkedin:<a>{linkedin}</a></p>
-        </div>
+            <p>Github: <a>{github}</a></p>
+            <p>Linkedin: <a>{linkedin}</a></p>
+        </Main>
     )
 }
 
---
+++ my-app/src/components/Funcionarios/Funcionarios.styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Main = styled.main`
+background-color: #0F1E1D;
+color:#5AEBD1;
+`
