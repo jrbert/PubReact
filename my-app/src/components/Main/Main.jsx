2f058a1c046797e738da6eb7bef312e95c83c5f3
jrbert <cabfjunior@gmail.com>
2021-08-29 19:48:38 -0300

    Pratos na pagina home finalizado

--- my-app/src/components/Main/Main.jsx
+++ my-app/src/components/Main/Main.jsx
@@ -7,7 +7,15 @@ const Main = () => {
             <figure id="imagemprincipal">
                 <img id="foto" src="https://i.ibb.co/mq4BrrQ/Resilia-Pub3.png" />
             </figure>
-            <div id="pratos"></div>
+            <div id="subtitulo"> <h2 class="prato">Pratos Principais</h2> </div> 
+            <div id="subtitulo2"> <h2 class="bebida">Bebidas</h2> </div>
+            <div class="linha1"><div id="linha"></div></div>
+            <div class="pratos">
+                <img id="prato1" src="https://i.ibb.co/23CFZrP/Roast-Beef.jpg"/>
+                <img id="prato2" src="https://i.ibb.co/wsvbxNg/Fish-And-Chips.jpg"/>
+                <img id="prato3" src="https://i.ibb.co/K6SCyZt/Bangers-And-Mash.jpg"/>
+                <img id="prato4" src="https://i.ibb.co/9sqZn85/Blue-Cheese-Burguer.jpg"/>
+            </div>
             <div id="aside"></div>
             <div id="info"></div>
         </Conteudo>
--- my-app/src/components/Main/Main.styles.js
+++ my-app/src/components/Main/Main.styles.js
@@ -8,45 +8,142 @@ export const Conteudo = styled.main`
         margin: 0; 
         padding: 0;
         box-sizing: border-box;
-        background: #7159c1;
+        background: #0F1E1D;
         display: grid;
         grid-template-columns: 3fr 1fr;
         grid-template-rows: auto;
-    
     
     #imagemprincipal {
         display: inline-block;
         grid-column-start: 1;
         grid-column-end: 4;
-        height: 500px;
+        height: 78vh;
     
     }
     #foto {
         width: 100%;
     }
-
-    #pratos {
+    #subtitulo {
+        display: inline-block;
+        align-items: center;
+        vertical-align: middle;
         grid-column-start: 1;
         grid-column-end: 2;
-        background: green;
-        height: 60vh;
+        grid-row-start: 2;
+        grid-row-end: 3;
+        height: 60px;
+        background-color: #0F1E1D;
+        color: #5AEBD1;
+        font-family: 'Playfair Display', serif;
+        font-weight: bold;
+        font-size: 18px;
+        padding-left: 20px;
+    }
+
+    #subtitulo2 {
+        display: inline-block;
+        align-items: center;
+        vertical-align: middle;
+        grid-column-start: 2;
+        grid-column-end: 4;
+        height: 60px;
+        background-color: #0F1E1D;
+        color: #5AEBD1;
+        font-family: 'Playfair Display', serif;
+        font-weight: bold;
+        font-size: 18px;
+    }
+    .linha1 {
+        background-color: #0F1E1D;
+        grid-column-start: 1;
+        grid-column-end:5;
+    }
+
+    #linha {
+        border-top: 2px;
+        border-style: solid;
+        border-color: gray;
+        grid-column-start: 1;
+        grid-column-end:5;
+        height: 10px;
+        background-color: #0F1E1D;
+        margin-left: 10vh;
+        margin-right: 10vh;
+            
+    }
+
+    .prato {
+        padding-top: 4vh;
+        padding-left: 7vh;
+    }
+
+    .bebida {
+        padding-top: 4vh;
+    }
+
+    .pratos {
+        display: flex;
+        flex-wrap: wrap;
+        grid-column-start: 1;
+        grid-column-end: 2;
+        grid-row-start: 5;
+        grid-row-end: 7;
+        background-color: #0F1E1D;
+        height: 92vh;
+        padding-left: 10vh;
+        padding-right: 10vh;
+        
+    }
+
+    #prato1 {
+        
+        display: flex;
+        width: 82vh;
+        height: 43vh;
+        padding-right: 3vh;
+    }
+
+    #prato2 {
+        
+        display: flex;
+        width: 56vh;
+        height: 43vh;
+        
+    }
+
+    #prato3 {
+        padding-right: 3vh;
+        display: flex;
+        width: 56vh;
+        height: 43vh;
+        
+    }
+
+    #prato4 {
+        display: flex;
+        width: 82vh;
+        height: 43vh;
     }
 
     #aside {
+        display: flex;
         grid-column-start: 2;
         grid-column-end: 3;
-        grid-row-start: 2;
-        grid-row-end: 4;
+        grid-row-start: 4;
+        grid-row-end: 8;
         background: blue;
+        padding-right: 20px;
         
     }
 
     #info {
         grid-column-start: 1;
         grid-column-end: 2;
+        grid-row-start: 7;
+        grid-row-end: 8;
         background: pink;
         height: 100px;
-}
+    }
 `
 
 
