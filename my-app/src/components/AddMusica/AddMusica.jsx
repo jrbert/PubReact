e35cb687cfd638bf01067a5cf29930142804ce85
GlaucoTolotto <glaucotolotto@gmail.com>
2021-08-30 21:09:15 -0300

    Estilizando a pagina de musicas; Criando um botão para adicionar musica

---
+++ my-app/src/components/AddMusica/AddMusica.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {AddMusicaButton, AddIcon} from './AddMusica.style.js'
+
+
+function AddMusica(){
+    return(
+        <div>
+            <AddMusicaButton>
+                <AddIcon/>
+            </AddMusicaButton>
+        </div>
+    )
+}
+
+export default AddMusica;
--- my-app/src/components/Music/Music.styles.js
+++ my-app/src/components/Music/Music.styles.js
@@ -1,24 +1,44 @@
 import styled from 'styled-components';
 
-export const Main = styled.main`
-background-color: #0F1E1D;
-
-`
-
 export const Caixa = styled.div`
-background-color: #004E67;
-margin-top: 5px;
-margin-bottom: 5px;
-padding: 25px;
-height: 100px;
-width: 200px;
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-color:#5AEBD1;
-`
-export const Container = styled.div`
+@import url('https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;0,600;1,400;1,600&display=swap%27');
+    background-color: #004E67;
+    margin: 5px 10px 10px 5px;
+    padding: 15px;
+    height: 150px;
+    width: 250px;
     display: flex;
-    justify-content: center;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: flex-start;
+    flex-wrap: nowrap;
+    color:#5AEBD1;
+    border-radius: 5px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.08);
+    
+    h1{
+        font-family: 'Playfair Display', serif;
+        font-weight: 600;
+    }
+
+    a{
+        text-decoration: none;
+        border-left: 3px solid #bbbbbb;
+        padding-left: 10px;
+        max-width: 230px;
+
+        color: #999999;
+        font-style: italic;
+        align-self: start;
+
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+
+        transition: 0.2s ease-out;
+
+        &:hover {
+            opacity: 0.5;
+        }
+}
 `
